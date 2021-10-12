110a699dacf69bc5413d49d0787e784e9191b3b4
Jermaine <jermainenee@yeah.net>
2021-10-12 11:02:46 +0800

    netflix模块 eureka client

--- Global-Utils/mybatis-plus/src/main/java/cn/zm/plus/config/MybatisPlusConfiguration.java
+++ Global-Utils/mybatis-plus/src/main/java/cn/zm/plus/config/MybatisPlusConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @Configuration
 @EnableCaching(proxyTargetClass = true)
-@MapperScan("cn.zm.plus.web.mapper")
+@MapperScan("cn.zm")
 public class MybatisPlusConfiguration {
     /**
      * 分页插件
--- Global-Utils/mybatis-plus/src/main/java/cn/zm/plus/web/mapper/AccountMapper.java
+++
@@ -1,8 +0,0 @@
-package cn.zm.plus.web.mapper;
-
-import cn.zm.plus.web.entity.Account;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface AccountMapper extends BaseMapper<Account> {
-
-}
