97d63d36da0683a6828f056a5cd2a951389a7b8b
Jermaine <jermainenee@yeah.net>
2021-10-13 16:31:10 +0800

    整合ribbon

---
+++ Spring-Cloud-Framwork/Spring-Cloud-Netflix/tk-mybatis-app/src/main/java/cn/zm/netflix/tkmybatis/web/service/IAccountService.java
@@ -0,0 +1,8 @@
+package cn.zm.netflix.tkmybatis.web.service;
+
+
+import cn.zm.netflix.tkmybatis.web.entity.Account;
+import cn.zm.tk.base.service.BaseService;
+
+public interface IAccountService extends BaseService<Account> {
+}
---
+++ Spring-Cloud-Framwork/Spring-Cloud-Netflix/tk-mybatis-app/src/main/java/cn/zm/netflix/tkmybatis/web/service/impl/AccountServiceImpl.java
@@ -0,0 +1,12 @@
+package cn.zm.netflix.tkmybatis.web.service.impl;
+
+import cn.zm.netflix.tkmybatis.web.service.IAccountService;
+import cn.zm.netflix.tkmybatis.web.entity.Account;
+import cn.zm.tk.base.service.BaseServiceImpl;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+// @Service
+@Transactional(rollbackFor = Exception.class)
+public class AccountServiceImpl extends BaseServiceImpl<Account> implements IAccountService {
+}
