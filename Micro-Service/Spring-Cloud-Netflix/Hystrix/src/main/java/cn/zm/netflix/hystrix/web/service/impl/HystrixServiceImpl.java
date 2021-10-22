a0cc3d6e0a2a34a5269f5bd127b9a5842f8bd4bc
Jermaine <jermainenee@yeah.net>
2021-10-22 10:37:31 +0800

    基于ribbon的熔断处理

--- Micro-Service/Spring-Cloud-Netflix/Hystrix/src/main/java/cn/zm/netflix/hystrix/web/service/impl/HystrixServiceImpl.java
+++ Micro-Service/Spring-Cloud-Netflix/Hystrix/src/main/java/cn/zm/netflix/hystrix/web/service/impl/HystrixServiceImpl.java
@@ -15,6 +15,19 @@ public class HystrixServiceImpl implements HystrixService {
     @Resource
     RestTemplate restTemplate;
 
+    /** 功能描述: <br>
+     * <@HystrixCommand注解。该注解对该方法创建了熔断器的功能，
+     * 并指定了fallbackMethod熔断方法，熔断方法直接返回了一个字符串，
+     * 字符串为{'info': 'hystrix handler'}，代码如下：
+     *
+     * @Service>
+     *
+     * @param
+     *
+     * @author 十渊
+     * @date 2021/10/22 10:28
+     * @return java.lang.String
+     */
     @HystrixCommand(fallbackMethod = "hystrixInfo")
     @Override
     public String ribbonHystrix() {
