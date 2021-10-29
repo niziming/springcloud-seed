b1e1ae9e43134e31d6c0a5b8205008721900c76b
Jermaine <jermainenee@yeah.net>
2021-10-29 13:19:36 +0800

    turbine

--- Micro-Service/Spring-Cloud-Netflix/Hystrix/src/main/java/cn/zm/HystrixApp.java
+++ Micro-Service/Spring-Cloud-Netflix/Hystrix/src/main/java/cn/zm/HystrixApp.java
@@ -8,14 +8,16 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 // 在工程的启动类中,通过@EnableDiscoveryClient向服务中心注册
 // @EnableHystrix注解开启Hystrix：
+// 在其入口类ServiceTurbineApplication加上注解@EnableTurbine，开启turbine，@EnableTurbine注解包含了@EnableDiscoveryClient注解，即开启了注册服务。
 @EnableHystrix
 @EnableHystrixDashboard
-@EnableDiscoveryClient
+@EnableTurbine
 @SpringBootApplication
 public class HystrixApp {
     public static void main(String[] args) {
