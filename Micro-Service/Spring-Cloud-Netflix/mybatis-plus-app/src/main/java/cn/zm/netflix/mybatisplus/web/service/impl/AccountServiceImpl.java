0ff24f362b20a3de209ae195aa367d10980b673d
Jermaine <jermainenee@yeah.net>
2021-11-17 15:38:28 +0800

    gateway 模块

--- Micro-Service/Spring-Cloud-Netflix/mybatis-plus-app/src/main/java/cn/zm/netflix/mybatisplus/web/service/impl/AccountServiceImpl.java
+++ Micro-Service/Spring-Cloud-Netflix/mybatis-plus-app/src/main/java/cn/zm/netflix/mybatisplus/web/service/impl/AccountServiceImpl.java
@@ -1,16 +1,22 @@
 package cn.zm.netflix.mybatisplus.web.service.impl;
 
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.util.ArrayUtil;
 import cn.zm.netflix.mybatisplus.web.entity.Account;
 import cn.zm.netflix.mybatisplus.web.entity.dto.AccountDTO;
 import cn.zm.netflix.mybatisplus.web.entity.vo.AccountVO;
 import cn.zm.netflix.mybatisplus.web.mapper.AccountMapper;
 import cn.zm.netflix.mybatisplus.web.service.IAccountService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import cn.zm.plus.utils.ConvertUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
