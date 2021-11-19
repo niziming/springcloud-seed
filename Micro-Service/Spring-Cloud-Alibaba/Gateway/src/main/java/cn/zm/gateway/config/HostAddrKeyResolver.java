2ffd42d00ef57a0396006c530880fc99a16c8514
Jermaine <jermainenee@yeah.net>
2021-11-19 17:29:59 +0800

    gateway 模块限流篇

--- Micro-Service/Spring-Cloud-Alibaba/Gateway/src/main/java/cn/zm/gateway/config/HostAddrKeyResolver.java
+++ Micro-Service/Spring-Cloud-Alibaba/Gateway/src/main/java/cn/zm/gateway/config/HostAddrKeyResolver.java
@@ -6,7 +6,7 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 /** 功能描述: <br>
- * <比如根据Hostname进行限流>
+ * <根据Hostname进行限流>
  *
  * @author 十渊
  * @date 2021/11/18 15:28
@@ -15,6 +15,7 @@ import reactor.core.publisher.Mono;
 public class HostAddrKeyResolver implements KeyResolver {
   @Override
   public Mono<String> resolve(ServerWebExchange exchange) {
+
     return Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
   }
 }
--- Micro-Service/Spring-Cloud-Alibaba/Gateway/src/main/java/cn/zm/gateway/config/UserKeyResolver.java
+++ Micro-Service/Spring-Cloud-Alibaba/Gateway/src/main/java/cn/zm/gateway/config/UserKeyResolver.java
@@ -4,7 +4,8 @@ import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-    /** 功能描述: <br>
+/**
+ * 功能描述: <br>
  * <以用户的维度去限流>
  *
  * @author 十渊
