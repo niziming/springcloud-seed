e726e83a2c9f29a8f3f0742de3eda6e2f4c3ef6c
Jermaine <jermainenee@yeah.net>
2022-07-24 21:32:12 +0800

    pom changes

--- related-framework/spring-security/src/main/java/cn/zm/security/SecurityApp.java
+++ related-framework/spring-security/src/main/java/cn/zm/security/SecurityApp.java
@@ -1,5 +1,6 @@
 package cn.zm.security;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author jermaine
  */
 @SpringBootApplication
+@MapperScan("cn.zm.**.mapper")
 // @SpringBootApplication(scanBasePackages = {"cn.zm"})
 public class SecurityApp {
   public static void main(String[] args) {
